1afac1faea9f1bc18d07b946de9038d3c2662942
Aaron Howser <aaronhowser1@gmail.com>
2023-03-20 00:10:59 -0500

    Tasklist is now a list of Tasks

--- Tasklist/task/src/tasklist/Main.kt
+++ Tasklist/task/src/tasklist/Main.kt
@@ -1,9 +1,9 @@
 package tasklist
 
 class Tasklist {
-    private val list = mutableListOf<String>()
+    private val list = mutableListOf<Task>()
 
-    fun add(task: String) = list.add(task)
+    fun add(task: Task) = list.add(task)
 
     fun remove(taskNumber: Int) {
         if (taskNumber in 1..list.size) list.removeAt(taskNumber-1)
@@ -12,17 +12,27 @@ class Tasklist {
     override fun toString(): String {
         if (list.size == 0) return "No tasks have been input."
         var output = ""
-        for (i in 0 until list.size) {
-            output += "${i+1} "
-            if (i in 0..8) output+= " "
-            output += "${list[i]}\n"
+        for (taskIndex in 0 until list.size) {
+            val task = list[taskIndex]
+            for (taskLine in 0 until task.lines.size) {
+                output += if (taskIndex == 0) taskLine+1 else " "
+                output += "  "
+                output += task.lines[taskLine]
+                output += "\n"
+                // Example:
+                // 1  Supermarket (taskLine[0])
+                //   -----------  (taskLine[1])
+                //   butter       (taskLine[2])
+                //   milk         (taskLine[3])
+                //   meat         (taskLine[4])
+            }
         }
         return output
     }
 }
 
 class Task(vararg lineArgs: String) {
-    private val lines = mutableListOf<String>()
+    val lines = mutableListOf<String>()
 
     init {
         for (line in lineArgs) lines.add(line)
